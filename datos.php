c397b18025609b5168ec46cb3fb628794d96fad3
ELWILY/ <86938707+ElWillyXd@users.noreply.github.com>
2022-05-17 21:51:47 -0300

    Update datos.php

--- datos.php
+++ datos.php
@@ -8,15 +8,15 @@ include_once('db.php');
 
 $nombre=$_POST['nombre'];
 $apellido=$_POST['apellido'];
-$email=$_POST['email'];
+$email=$_POST['correo'];
 $mensaje=$_POST['mensaje'];
 
 echo "Los datos son los siguientes: <br>";
 echo "$nombre, $apellido, $email y $mensaje";
 
 $conectar=datos(); //Ejeculas las conexiones con base de datos
-$sql="INSERT INTO datosweb (nombre, apellido, email, mensaje)
-VALUES ('$nombre', '$apellido', '$email', '$mensaje')";
+$sql="INSERT INTO datosweb (nombre, apellido, correo, mensaje)
+VALUES ('$nombre', '$apellido', '$correo', '$mensaje')";
 $resulltado = mysqli_query($conectar, $sql) or trigger_error("Query Fallo!" .mysqli_error($conectar), E_USER_ERROR);
 
 echo "$sql";
